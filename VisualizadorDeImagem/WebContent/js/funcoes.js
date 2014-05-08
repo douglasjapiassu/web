b01552a644cedc8a477458260ab362d0f7771e3a
douglasjapiassu <douglas.japiassu@gmail.com>
2014-05-08 15:14:17 -0300

    Alterações no leiaute da pagina.

--- VisualizadorDeImagem/WebContent/js/funcoes.js
+++ VisualizadorDeImagem/WebContent/js/funcoes.js
@@ -1,193 +1,158 @@
 window.onload = function() {
-	//setando o tamanho máximo da div que recebe a imagem, afim de não criar barra de rolagem.
-	document.getElementById('divImagem').style.maxWidth = window.innerWidth + "px";
-	document.getElementById('divImagem').style.maxHeight = (window.innerHeight - 20) + "px";
 	
-	//declarando os eventos do mouse para arrastar a imagem
+	if (isInternetExplorer()) {
+		document.getElementById('inpLocalArquivo').disabled = true;
+	}
+	
+	//setando o tamanho máximo da div que recebe a imagem, afim de não criar barra de rolagem.
+	document.getElementById('fsImagem').style.maxWidth = window.innerWidth + "px";
+	document.getElementById('fsImagem').style.maxHeight = (window.innerHeight - 100) + "px";
+	document.getElementById('fsImagem').style.height = (window.innerHeight - 100) + "px";
+	
+	//setando os eventos de click do mouse
 	document.getElementById('imagem').onmousedown = startDrag;
 	document.getElementById('imagem').onmouseup = stopDrag;
 	
-	//declarando o evento para carregar o arquivo
+	//setando os eventos de zoom
+	document.getElementById('btnZoomIn').onclick = fnZoomIn;
+	document.getElementById('btnZoomOut').onclick = fnZoomOut;
+	
+	document.getElementById('inpUrlImagem').onclick = mostrarImagemPelaUrl;
+	
+	//ação executada ao selecionar alguma imagem
 	document.getElementById('inpLocalArquivo').onchange = function() {
 		if (this.files[0].type != 'image/png' && this.files[0].type != 'image/jpeg') {
 			alert("São permitidas apenas as extensões 'png' e 'jpeg'");
 			return;
 		}
-			
+		
 		mostrarImagem(this);
-		this.style.display = 'none';
 	};
 };
 
-function mostrarImagem(imagem) {
-	var reader = new FileReader();
-    reader.onload = function (e) {
-		document.getElementById('imagem').src = e.target.result;
-    };
-    reader.readAsDataURL(imagem.files[0]);
+function isInternetExplorer() {
+    var navegador = navigator.appName.toLowerCase();
+    
+    if (navegador.indexOf("explorer") != -1) {
+    	return true;
+    }
+    
+    return false;
 }
 
-function startDrag(evt) {
-	// determine event object
-	if (!evt) {
-		var evt = window.event;
+function mostrarImagem(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+	    reader.onload = function (e) {
+			document.getElementById('imagem').src = e.target.result;
+	    };
+	    reader.readAsDataURL(input.files[0]);
+	} else {
+		var img = input.value;
+		document.getElementById('imagem').src = img;
 	}
 	
-    if(evt.preventDefault) evt.preventDefault();
+	heightInicial = document.getElementById('imagem').height;
+	widthInicial = document.getElementById('imagem').heigth;
+	
+	document.getElementById('btnZoomIn').disabled = false;
+	document.getElementById('btnZoomOut').disabled = false;
+}
 
-	var elemento = evt.target ? evt.target : evt.srcElement;
+function startDrag(mouseEvt) {
+	var evento = mouseEvt;
+	
+	if (!evento) {
+		evento = window.event;
+	}
+	
+    if(evento.preventDefault) evento.preventDefault();
 
-	if (elemento.className != 'dragme')
+	var elemento = evento.target ? evento.target : evento.srcElement;
+
+	if (elemento.id != 'imagem')
 		return;
 	
-	// calculate event X, Y coordinates
-	offsetX = evt.clientX;
-	offsetY = evt.clientY;
+	//pega as coordenadas do ponteiro do mouse
+	offsetX = evento.clientX;
+	offsetY = evento.clientY;
 
-	// setando valores iniciais
+	//inicializando valores
 	if (!elemento.style.left) elemento.style.left='0px';
 	if (!elemento.style.top) elemento.style.top='0px';
 
-	// calculate integer values for top and left 
-	// properties
+	
 	coordX = parseInt(elemento.style.left);
 	coordY = parseInt(elemento.style.top);
 	
-	areaHorizontal = document.body.clientWidth - elemento.clientWidth - 2;
-    areaVertical = document.body.clientHeight - elemento.clientHeight - 2;
-
 	drag = true;
-	document.onmousemove = arrastarImagem;
+	
+	//evento de arrastar a imagem, definindo a posição
+	document.onmousemove = definirPosicaoImagem;
 	
     return false;
 }
 
-/**
- * Arrastar o elemento
- */
-function arrastarImagem(e) {
+function definirPosicaoImagem(mouseEvt) {
+	var evento = mouseEvt;
+	
+	if (!evento) {
+		evento = window.event;
+	}
+	
 	if (!drag) return;
 	
-	if (!e) var e = window.event;
+	//elemento clicado
+	var elemento = mouseEvt.target? evento.target : evento.srcElement;
 	
-	var targ = e.target? e.target : e.srcElement;
+	if (elemento.id != 'imagem')
+		return;
 	
-	//movendo o elemento
-	targ.style.left = coordX + e.clientX - offsetX + 'px';
-	targ.style.top = coordY + e.clientY- offsetY + 'px';
+	//nova posição será a posição inicial da imagem (coord) + posição em que o mouse 
+	//se encontra (mouseEvt.client) - posição inicial do mouse (offset)
+	elemento.style.left = coordX + evento.clientX - offsetX + 'px';
+	elemento.style.top = coordY + evento.clientY- offsetY + 'px';
+	
 	return false;
 }
 
 function stopDrag() {
-	drag=false;
+	drag = false;
 }
 
-/*############################
-
-var objSelecionado = null;
-var mouseOffset = null;
-/*function addEvent(obj, evType, fn) {
-    if (typeof obj == "string") {
-        if (null == (obj = document.getElementById(obj))) {
-            throw new Error("Elemento HTML não encontrado. Não foi possível adicionar o evento.");
-        }
-    }
-    if (obj.attachEvent) {
-        return obj.attachEvent(("on" + evType), fn);
-    } else if (obj.addEventListener) {
-        return obj.addEventListener(evType, fn, true);
-    } else {
-        throw new Error("Seu browser não suporta adição de eventos. Senta, chora e pega um navegador mais recente.");
-    }
-}
-function mouseCoords(ev){
-    if(typeof(ev.pageX)!=="undefined"){
-        return {x:ev.pageX, y:ev.pageY};
-    }else{
-        return {
-            x:ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-            y:ev.clientY + document.body.scrollTop  - document.body.clientTop
-        };
-    }
-}
-function getPosition(e, ev){
-    var ev = ev || window.event;
-    if(e.constructor==String){ e = document.getElementById(e);}
-    var left = 0, top  = 0;
-    var coords = mouseCoords(ev);    
-
-    while (e.offsetParent){
-        left += e.offsetLeft;
-        top  += e.offsetTop;
-        e     = e.offsetParent;
-    }
-    left += e.offsetLeft;
-    top  += e.offsetTop;
-    return {x: coords.x - left, y: coords.y - top};
+function fnZoomIn() {
+	document.getElementById('imagem').height = document.getElementById('imagem').height * 1.1;
+	document.getElementById('imagem').width = document.getElementById('imagem').width * 1.1;
 }
 
-function dragdrop(local_click, caixa_movida) {
-    //local click indica quem é o cara que quando movido, move o caixa_movida
-    if(local_click.constructor==String){ local_click = document.getElementById(local_click);}
-    if(caixa_movida.constructor==String){ caixa_movida = document.getElementById(caixa_movida);}
+function fnZoomOut(evento) {
+	document.getElementById('imagem').height = document.getElementById('imagem').height / 1.1;
+	document.getElementById('imagem').width = document.getElementById('imagem').width / 1.1;
+}
 
-    local_click.style.cursor = 'move';
-    if(!caixa_movida.style.position || caixa_movida.style.position=='static'){
-        caixa_movida.style.position='relative'
-    }
-    local_click.onmousedown = function(ev) {
-        objSelecionado = caixa_movida;
-        mouseOffset = getPosition(objSelecionado, ev);
-        if(mouseOffset < 0){objSelecionado.style.margin = '50px';}
-    };
-    document.onmouseup = function() {
-        objSelecionado = null;
-    }
-    document.onmousemove = function(ev) {
-        if (objSelecionado) {
-            var ev = ev || window.event;
-            var mousePos = mouseCoords(ev);
-            var pai = objSelecionado.parentNode;
+function mostrarImagemPelaUrl(evento) {
+	var srcImagem = prompt('Insira a URL');
+	
+	//click cancelar
+	if (srcImagem == null) return;
+	
+	if (srcImagem != '') {
+		document.getElementById('imagem').src = srcImagem;
+		document.getElementById('inpLocalArquivo').files = [];
+	} else {
+		alert("Insira uma Url valida!");
+	}
+}
 
-
-
-            //as variáveis w e h definem a posição do(s) objeto(s) movido(s)
-
-            var w = (mousePos.x - mouseOffset.x - pai.offsetLeft);
-            var h = (mousePos.y - mouseOffset.y - pai.offsetTop);
-
-
-
-            //as variáveis areaHorizontal e areaVertical definem a área maxima, onde o objeto será movido (nesse caso é a area maxima do browser)
-
-            var areaHorizontal = document.body.clientWidth - objSelecionado.clientWidth - 2;
-            var areaVertical = document.body.clientHeight - objSelecionado.clientHeight - 2;
-            
-            objSelecionado.style.left = w + 'px';
-            objSelecionado.style.top = h + 'px';
-
-
-
-            //essa é a estrutura de condição que reposiciona o objeto movido para não ultrapassar a área máxima
-            if(w >= areaHorizontal){
-                objSelecionado.style.left = areaHorizontal;
-            }
-            if(w <= 0){
-                objSelecionado.style.left = 0;
-            }
-            if(h >= areaVertical){
-                objSelecionado.style.top = areaVertical;
-            }
-            if(h <= 0){
-                objSelecionado.style.top = 0;
-            }
-
-
-
-            objSelecionado.style.margin = '0px';
-            return false;
-        }
-    }
-}*/
+function clickRadio(radio) {
+	if (radio.value == 'local') {
+		document.getElementById('inpLocalArquivo').style.display = 'block';
+		document.getElementById('inpUrlImagem').style.display = 'none';
+	} else {
+		document.getElementById('inpUrlImagem').style.display = 'block';
+		document.getElementById('inpLocalArquivo').style.display = 'none';
+	}
+		
+}
 
 //@ source=funcoes.js
